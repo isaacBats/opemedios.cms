276d6e2d61f5646b78fcb68a1588f625da4d5304
isaacBats <disa.indus@gmail.com>
2015-12-06 15:36:40 -0600

    Segunda prueba de enviar email

--- pruebamail.php
+++ pruebamail.php
@@ -6,11 +6,14 @@ include ("lib/PHPMailer/class.smtp.php");
 $phpmailer = new PHPMailer();
 $phpmailer->isSMTP();
 $phpmailer->SMTPAuth = true;
-$phpmailer->Host = 'email-smtp.us-west-2.amazonaws.com';
+//$phpmailer->Host = 'email-smtp.us-west-2.amazonaws.com';
+$phpmailer->Host = 'smtp.gmail.com';
 $phpmailer->Port = 25;
-$phpmailer->Username = 'AKIAI5J7FY3GTCNC4UUA';
-$phpmailer->Password = 'Ao/BnhPkCPjlKEZx/hvUeX4MBstNR8BNyWPt3X9IOSCK';
-$phpmailer->SetFrom('noticias@opemedios.com.mx', 'Noticias OPEMEDIOS');
+//$phpmailer->Username = 'AKIAI5J7FY3GTCNC4UUA';
+//$phpmailer->Password = 'Ao/BnhPkCPjlKEZx/hvUeX4MBstNR8BNyWPt3X9IOSCK';
+$phpmailer->Username = 'noticias.opemedios@gmail.com';
+$phpmailer->Password = 'noti1234';
+//$phpmailer->SetFrom('noticias@opemedios.com.mx', 'Noticias OPEMEDIOS');
 
 $phpmailer->AddAddress('klonate@gmail.com', 'daniel@carlos-villicana-davila.com.mx');
 $phpmailer->Subject = 'Test envio autenticado';
