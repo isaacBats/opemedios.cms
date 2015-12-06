e931afc05adb212d83bb626bfd332e743ab18be1
isaacBats <disa.indus@gmail.com>
2015-12-05 18:20:08 -0600

    hotfix en pruebamail.php con asignacion de valor al objeto phpmailer

--- pruebamail.php
+++ pruebamail.php
@@ -1,19 +1,19 @@
 <?php 
 
-require "lib/PHPMailer/class.phpmailer.php";
+include ("lib/PHPMailer/class.phpmailer.php");
 
 $phpmailer = new PHPMailer();
-$phpmailer->isSMTP()
-          ->SMTPAuth = true
-          ->Host = 'email-smtp.us-west-2.amazonaws.com'
-          ->Port = 25
-          ->Username = 'AKIAI5J7FY3GTCNC4UUA'
-          ->Password = 'Ao/BnhPkCPjlKEZx/hvUeX4MBstNR8BNyWPt3X9IOSCK'
-          ->SetFrom('noticias@opemedios.com.mx', 'Noticias OPEMEDIOS');
+$phpmailer->isSMTP();
+$phpmailer->SMTPAuth = true;
+$phpmailer->Host = 'email-smtp.us-west-2.amazonaws.com';
+$phpmailer->Port = 25;
+$phpmailer->Username = 'AKIAI5J7FY3GTCNC4UUA';
+$phpmailer->Password = 'Ao/BnhPkCPjlKEZx/hvUeX4MBstNR8BNyWPt3X9IOSCK';
+$phpmailer->SetFrom('noticias@opemedios.com.mx', 'Noticias OPEMEDIOS');
 
-$phpmailer->AddAddress('klonate@gmail.com', 'daniel@carlos-villicana-davila.com.mx')
-          ->Subject = 'Test envio autenticado'
-          ->MsgHTML('<h1>Enviando E-mail autenticado</h1>');
+$phpmailer->AddAddress('klonate@gmail.com', 'daniel@carlos-villicana-davila.com.mx');
+$phpmailer->Subject = 'Test envio autenticado';
+$phpmailer->MsgHTML('<h1>Enviando E-mail autenticado</h1>');
 
 if($phpmailer->Send())
 	echo 'E-mail enviado con exito';
