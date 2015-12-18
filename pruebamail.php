f9984c57fcd5e42febcbd5f69e5b8a44d960662c
isaacBats <disa.indus@gmail.com>
2015-12-18 00:06:17 -0600

    Prueba 2 de correo

--- pruebamail.php
+++ pruebamail.php
@@ -6,16 +6,16 @@ include ("lib/PHPMailer/class.smtp.php");
 $phpmailer = new PHPMailer();
 $phpmailer->isSMTP();
 $phpmailer->SMTPAuth = true;
-//$phpmailer->Host = 'email-smtp.us-west-2.amazonaws.com';
-$phpmailer->Host = 'smtp.gmail.com';
+$phpmailer->Host = 'email-smtp.us-west-2.amazonaws.com';
+//$phpmailer->Host = 'smtp.gmail.com';
 $phpmailer->Port = 25;
-//$phpmailer->Username = 'AKIAI5J7FY3GTCNC4UUA';
-//$phpmailer->Password = 'Ao/BnhPkCPjlKEZx/hvUeX4MBstNR8BNyWPt3X9IOSCK';
-$phpmailer->Username = 'noticias.opemedios@gmail.com';
-$phpmailer->Password = 'noti1234';
-//$phpmailer->SetFrom('noticias@opemedios.com.mx', 'Noticias OPEMEDIOS');
+$phpmailer->Username = 'AKIAI5J7FY3GTCNC4UUA';
+$phpmailer->Password = 'Ao/BnhPkCPjlKEZx/hvUeX4MBstNR8BNyWPt3X9IOSCK';
+//$phpmailer->Username = 'noticias.opemedios@gmail.com';
+//$phpmailer->Password = 'noti1234';
+$phpmailer->SetFrom('noticias@opemedios.com.mx', 'Noticias OPEMEDIOS');
 
-$phpmailer->AddAddress('klonate@gmail.com', 'daniel@carlos-villicana-davila.com.mx');
+$phpmailer->AddAddress('klonate@gmail.com', 'daniel@carlos-villicana-davila.com.mx', 'prueba@opemedios.com.mx');
 $phpmailer->Subject = 'Test envio autenticado';
 $phpmailer->MsgHTML('<h1>Enviando E-mail autenticado</h1>');
 
