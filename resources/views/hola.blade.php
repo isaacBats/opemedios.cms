8d1a28ba2b4dc4a0897c606c5f19051967dfd6d8
isaacBats <disa.indus@gmail.com>
2016-02-26 00:30:29 -0600

    probando template

--- resources/views/hola.blade.php
+++ resources/views/hola.blade.php
@@ -2,6 +2,13 @@
 
 @section('content')
     <p>This is my body content. Of HOLA</p>
+    <div>
+    	<ul>
+    		<li>Uno</li>
+    		<li>Dos</li>
+    		<li>Tres</li>
+    	</ul>
+    </div>
 
     <p>{{ $message }}</p>
 @stop
